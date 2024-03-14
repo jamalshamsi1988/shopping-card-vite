69677f41ef1241e5f1c7b10437b75bccc2857763
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-03-15 01:19:16 +0330

    create categories

--- vite-project/src/pages/ProductsPage.jsx
+++ vite-project/src/pages/ProductsPage.jsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from 'react';
+import {ImSearch} from 'react-icons/im'
+import {FaListUl} from 'react-icons/fa'
+
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext"
@@ -5,20 +9,53 @@ import styles from './ProductsPage.module.css'
 
 const ProductsPage = () => {
   const products=useProducts();
-  console.log(products)
+  const [displayed,setDisplayed]=useState([]);
+  const [search,setSearch]=useState("");
+
+  useEffect(()=>{
+    setDisplayed(products)
+  },[products])
+  const searchHandler=()=>{
+
+  }
+
+const categoryHandler=(e)=>{
+  const {tagName}=e.target;
+const category = e.target.innerText.toLowerCase();
+
+  if(tagName !== "LI") return;
+}
   return (
+    <>
+    <div>
+      <input type='text' placeholder='Search ...' value={search} onChange={e => setSearch(e.target.value.toLowerCase().trim())} />
+   <button onClick={searchHandler}><ImSearch /></button>
+    </div>
     <div className={styles.container}>
       {
-        !products.length && <Loader />
+        !displayed.length && <Loader />
       }
       <div className={styles.products}>
         {
-          products.map(p => <Card key={p.id} data={p} />)
+          displayed.map(p => <Card key={p.id} data={p} />)
         }
       </div>
-      <div>SideBar</div>
+      <div>
+        <div><FaListUl />
+        <p>Categories</p>
+        </div>
+        <ul onClick={categoryHandler}>
+          <li>All</li>
+          <li>Electronics</li>
+          <li>Jewelery</li>
+          <li>Men's Clothing</li>
+          <li>Women's Clothing</li>
+
+        </ul>
+      </div>
 
     </div>
+    </>
   )
 }
 
