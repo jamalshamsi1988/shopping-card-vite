4ad3f3e272148fa2ff4d8a5a3c1efb84f803d417
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-03-25 09:51:39 +0430

    show data in basket card

--- vite-project/src/pages/ChechOutPage.jsx
+++ vite-project/src/pages/ChechOutPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import BasketCard from "../components/BasketCard";
+import { useCard } from "../context/CardContext"
 
 const ChechOutPage = () => {
+  const [state,dispatch]=useCard();
+
+  const clickHandler=(type,payload)=> dispatch({type,payload})
+
   return (
     <div>
-      ChechOutPage
+      <div>
+        {
+          state.selectedItems.map(product => (<BasketCard key={product.id} data={product} clickHandler={clickHandler} />))
+        }
+      </div>
     </div>
   )
 }
