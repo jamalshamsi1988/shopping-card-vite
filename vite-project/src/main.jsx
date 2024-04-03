cddcb1e5e3909cac9a64f2f706c0af416deecbdb
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-04-03 10:05:33 +0430

    create store and use store in main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx'
-import './global.css'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
+import App from "./App.jsx";
+import "./global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
   //</React.StrictMode>,
-)
+);
