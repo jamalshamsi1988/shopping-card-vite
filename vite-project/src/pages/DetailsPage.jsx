fbe3c2c464e4287d40d3ebcd24f84cc9ad49d3fc
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-04-04 10:06:32 +0430

    details page  codes  were replaced  with redduxToolkit

--- vite-project/src/pages/DetailsPage.jsx
+++ vite-project/src/pages/DetailsPage.jsx
@@ -1,30 +1,41 @@
 import { Link, useParams } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
 import {SiOpenproject} from 'react-icons/si'
 import { IoMdPricetag } from "react-icons/io";
 import { FaArrowLeft } from "react-icons/fa";
 
-import styles from './DetailsPage.module.css'
-import { useProductsDetails } from "../context/ProductContext"
+import { fetchProducts } from "../features/product/productSlice";
 import Loader from "../components/Loader"
+import styles from './DetailsPage.module.css'
+// import { useProductsDetails } from "../context/ProductContext"
 
 const DetailsPage = () => {
   const {id}= useParams()
-  const productDetails=useProductsDetails(+id);
-  const {title,image,price,description,category}=productDetails;
+  // const productDetails=useProductsDetails(+id);
+  const dispatch=useDispatch();
+  useEffect(()=>{
+    dispatch(fetchProducts())
+  },[])
+  const productDetails=useSelector(store => store.product.products.find(i => i.id === +id));
+
+
+  // const {title,image,price,description,category}=productDetails;
   if(!productDetails ) return <Loader />
   return (
     <div className={styles.container} >
-      <img src={image} alt={title} />
+      <img src={productDetails.image} alt={productDetails.title} />
       <div className={styles.information}>
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
-      <p className={styles.category}><SiOpenproject/> {category}</p>
+      <h3 className={styles.title}>{productDetails.title}</h3>
+      <p className={styles.description}>{productDetails.description}</p>
+      <p className={styles.category}><SiOpenproject/> {productDetails.category}</p>
       
 
       <div>
         <span className={styles.price}>
           <IoMdPricetag />
-          {price} $
+          {productDetails.price} $
         </span>
         <Link to='/products'>
           <FaArrowLeft/>
